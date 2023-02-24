3353b434b928f06f865fffa3ed48396851f4e08d
William Spacefire <git@williamspacefire.com>
2023-02-24 01:56:17 -0300

    Fixed: implicitly has an 'any' type. during building

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 // pages/_app.js
 import { ChakraProvider } from "@chakra-ui/react"
+import { AppProps } from "next/app"
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ChakraProvider>
             <Component {...pageProps} />
